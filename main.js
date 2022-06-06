178835ec1db514094d82591f2c4c4ac89cdc1303
dheerapat <dheerapat.t@kkumail.com>
2022-06-06 21:45:03 +0700

    initial commit

---
+++ main.js
@@ -0,0 +1,15 @@
+const yesBtn = document.getElementById('yes');
+const noBtn = document.getElementById('no');
+const main = document.getElementById('main');
+const ans = document.getElementById('reveal');
+
+noBtn.onclick = function () {
+    noBtn.style.visibility = 'hidden';
+};
+
+yesBtn.onclick = function () {
+    ans.style.display = 'block';
+    main.style.display = 'none';
+    yesBtn.style.visibility = 'hidden';
+    noBtn.style.visibility = 'hidden';
+}
